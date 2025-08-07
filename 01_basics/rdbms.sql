f8f063ae6e398e06e9afdb851c3ceef978d3f653
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-08-07 07:25:40 +0200

    Add files via upload

---
+++ 01_basics/rdbms.sql
@@ -0,0 +1,14 @@
+\! cls -- clear screen
+
+# Zeilenkommentar
+-- Zeilenkommentar
+/* Blockkommentar */
+
+-- Datenbanken anzeigen
+SHOW DATABASES;
+
+-- Zur DB mysql wechseln
+USE mysql;
+
+-- Tabellen anzeigen
+SHOW TABLES;

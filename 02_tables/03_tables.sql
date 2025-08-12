31ccc5358c51eccbe69331a2c3ef8be536d3ac94
talgas <renedueren3@yahoo.de>
2025-08-12 11:35:19 +0200

    hash

--- 02_tables/03_tables.sql
+++ 02_tables/03_tables.sql
@@ -8,15 +8,16 @@ CREATE TABLE IF NOT EXISTS kunde
     k_id INT AUTO_INCREMENT PRIMARY KEY,
     k_name VARCHAR(45) NOT NULL,
     k_alter TINYINT,
-    k_mail VARCHAR(100) UNIQUE
+    k_mail VARCHAR(100) UNIQUE,
+    k_pw VARCHAR(100)
 );
 
 SHOW TABLES;
 
 DESCRIBE kunde;
 
-INSERT INTO kunde(k_id,k_name, k_alter,k_mail) VALUES(DEFAULT,"Meier",28, "meier@web.de");
-INSERT INTO kunde(k_id,k_name, k_alter,k_mail) VALUES(DEFAULT,"Meier", 40, "meier1@web.de");
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT, NULL, NULL, NULL);
+INSERT INTO kunde(k_id,k_name, k_alter, k_mail, k_pw) VALUES(DEFAULT,"Meier", 28, "meier@web.de",SHA2("test", 256));
+INSERT INTO kunde(k_id,k_name, k_alter, k_mail, k_pw) VALUES(DEFAULT,"Meier", 40, "meier1@web.de",SHA2("Blume", 256));
+INSERT INTO kunde(k_id,k_name, k_alter, k_mail, k_pw) VALUES(DEFAULT, "Fischer", 30, "fischer@meier.de",SHA2( "123456", 256));
 
 SELECT * FROM kunde;
